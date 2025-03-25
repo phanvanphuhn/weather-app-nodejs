f7c1ccca9a571447464a3b0e9750ea27730db061
Jeff Phan <nostarwhere.pc@gmail.com>
2025-03-25 11:21:08 +0700

    add web-server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.get("", (req, res) => {
+  res.send("<h1>Weather</h1>");
+});
+
+app.get("/help", (req, res) => {
+  res.send({
+    name: "John",
+    age: 20,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is sunny",
+    location: "Philadelphia",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
