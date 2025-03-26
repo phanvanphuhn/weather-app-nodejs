81974dad58dc076b3d18090839b1217ccf4d5941
Jeff Phan <nostarwhere.pc@gmail.com>
2025-03-26 11:13:38 +0700

    add header, footer of hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -16,14 +20,34 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead",
+    name: "Weather page",
+    footerText: "This is a footer",
   });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutText: "This is about page",
+    title: "About",
+    name: "About page",
+    footerText: "This is a footer",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is a help message",
+    title: "Help",
+    name: "Help page",
+    footerText: "This is a footer",
+    });
 });
 
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is sunny",
     location: "Philadelphia",
+    footerText: "This is a footer",
   });
 });
 
