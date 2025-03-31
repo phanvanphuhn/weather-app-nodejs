4941da3a4c8ec42df19a8ece599a5817a413bc94
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-04-01 06:34:18 +0700

    add update func

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -48,6 +48,34 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -73,6 +101,34 @@ app.get("/tasks/:id", async (req, res) => {
 
   try {
     const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     if (!task) {
       return res.status(404).send();
     }
