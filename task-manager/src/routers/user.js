0da7bdebc4471585192f33e306cf884b63ee5c57
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-04-01 22:38:03 +0700

    add login func

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -13,6 +13,18 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -51,10 +63,10 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(_id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+
     if (!user) {
       return res.status(404).send();
     }
