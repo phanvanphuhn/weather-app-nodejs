194f96d2198d7349e642f7e2fec9e190f0b30c61
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-04-10 23:21:33 +0700

    add query param

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,9 +18,28 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
+  const sort = {};
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    res.send(tasks);
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
   }
