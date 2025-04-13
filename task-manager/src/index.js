11a65fc10dcf4e38889eb7aaf40bb0b418551b7f
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-04-13 07:20:06 +0700

    add env

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,7 +7,7 @@ const taskRouter = require("./routers/task");
 
 // App
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const multer = require("multer");
 const upload = multer({
